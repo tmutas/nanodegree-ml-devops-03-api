fa973f22d4d1431ad20c11fa4f7a581f44f52bb4
Timo Mutas <tmutas@users.noreply.github.com>
2022-06-16 09:15:44 +0200

    Add tests for model training

---
+++ starter/tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+import pandas as pd
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--rawdata_path",
+        action="store",
+        default="tests/test_rawdata.csv",
+        required=False)
+
+@pytest.fixture
+def cat_features():
+    return [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+@pytest.fixture
+def random_forest_config():
+    conf = {
+        "n_estimators" : 10,
+        "max_depth" : 5
+    }
+    return conf
+
+@pytest.fixture
+def rawdata_full(request):
+    rawdata_path = request.config.option.rawdata_path
+    df = pd.read_csv(rawdata_path)
+    return df
+
+@pytest.fixture(params = [1,2,42])
+def rawdata_samples(request, rawdata_full):
+    return rawdata_full.sample(random_state = request.param)
---
+++ starter/tests/test_data.py
@@ -0,0 +1,20 @@
+import starter.ml.data as data
+
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+
+def test_train_test_split(rawdata_samples, cat_features):
+    X, y, encoder, lb = data.process_data(
+        rawdata_samples,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    assert len(X) == len(y)
+
+    # Test dimensions of processed data
+    assert len(X.shape) == 2
+    assert len(y.shape) == 1
+
+    assert isinstance(encoder, OneHotEncoder)
+    assert isinstance(lb, LabelBinarizer)
