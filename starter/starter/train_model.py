7598873edbb6f8e64e51d1233693033a1149bfe4
Timo Mutas <tmutas@users.noreply.github.com>
2022-07-10 19:02:29 +0200

    Change to also save categorical features list

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -59,7 +59,12 @@ def run(args):
 
     logging.debug("Model trained")
 
-    artifacts = {"model": model, "encoder": encoder, "labelbinarizer": lb}
+    artifacts = {
+        "model": model,
+        "encoder": encoder,
+        "labelbinarizer": lb,
+        "categorical_features":cat_features
+    }
     if args.artifact_path is not None:
         save_model(args.artifact_path, artifacts)
         logging.debug(f"Artifacts saved to {args.artifact_path}")
